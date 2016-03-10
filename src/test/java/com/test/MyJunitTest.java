e7281403dda500b7c698c072ff448a1b68d75fd8
priyanka.e.gupta <priyanka.e.gupta@accenture.com>
2016-03-10 17:51:37 +0530

    commit

--- src/test/java/com/test/MyJunitTest.java
+++ src/test/java/com/test/MyJunitTest.java
@@ -8,7 +8,7 @@ public class MyJunitTest {
 
 
     @Test
-    public void testConcatenate() {
+    public oid testConcatenate() {
         MyJUnit myUnit = new MyJUnit();
 
         String result = myUnit.concatenate("one", "two");
